cb3e0262a6c64a1644fd7f1919b90c9d2000606e
Rhys Adams (home) <rhys@therhys.co.uk>
2022-04-26 17:26:36 +0100

    Corrected valgrind errors.

--- src/main/header.c
+++ src/main/header.c
@@ -21,11 +21,14 @@ Headers_t* headers_append(Headers_t *headers, const char *key, const char* value
 
     headers->length++;
 
-    headers->keys = realloc(headers->keys, headers->length);
-    headers->values = realloc(headers->values, headers->length);
+    headers->keys = realloc(headers->keys, (headers->length+1)*sizeof(char*));
+    headers->values = realloc(headers->values, (headers->length+1)*sizeof(char*));
 
-    headers->keys[headers->length-1] = calloc(strlen(key)+1, sizeof(char));strcpy(headers->keys[headers->length-1], key);
-    headers->values[headers->length-1] = calloc(strlen(value)+1, sizeof(char));strcpy(headers->values[headers->length-1], value);
+    headers->keys[headers->length-1] = calloc(strlen(key)+1, sizeof(char));
+    strcpy(headers->keys[headers->length-1], key);
+
+    headers->values[headers->length-1] = calloc(strlen(value)+1, sizeof(char));
+    strcpy(headers->values[headers->length-1], value);
 
     return headers;
 }
