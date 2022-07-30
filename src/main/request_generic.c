1336962ecc7b8c1f95f99a1630211116aebe4142
Rhys Adams <rhys@therhys.co.uk>
2022-07-30 16:08:42 +0100

    Fixed when req_get_dl cannot open file there would be a crash.

--- src/main/request_generic.c
+++ src/main/request_generic.c
@@ -12,6 +12,10 @@ LIBRARY_API size_t req_get_dl(const char* url, const char* path, int use_proxy, 
     Res_t* out = req_get(url, use_proxy, proxy, headers);
 
     FILE* fout = fopen(path, "wb");
+    if(fout == NULL) {
+        printf("Cannot open file '%s'!\n", path);
+        return 1;
+    }
 
     fwrite(out->data, out->length, sizeof(char), fout);
 
